f44710e535f84f7a5e4c85957c2bf2960aaee3f9
CARD student dummy account <cardstud@dmlab09.(none)>
2015-03-19 14:12:39 +0100

    fixed Makefile, started on making driver

--- ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,30 +5,47 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/ioport.h>
 
-/*
- * template_init - function to insert this module into kernel space
- *
- * This is the first of two exported functions to handle inserting this
- * code into a running kernel
- *
- * Returns 0 if successfull, otherwise -1
- */
+#define PORTA_BASE  0x40006000
+#define PORTC_BASE  0x40006048
+#define PORT_LENGTH (PORTA_BASE - PORTZ_BASE)
+
+static int my_open(struct inode *inode, struct file *filp);
+static int my_release(struct inode *inode, struct file *filp);
+static ssize_t my_read( struct file *filp, 
+                            char __user *buff,
+                            size_t count, loff_t *offp);
+
+static ssize_t my_write(struct file *filp,
+                        const char __user *buff,
+                        size_t count,
+                        loff_t offp);
+
+struct cdev my_cdev;
+struct class *cl;
+dev_t devno;
+
+static struct file_operations myfops ={
+    .owner = THIS_MODULE;
+    .read = my_read;
+    .write = my_write;
+    .open = my_open;
+    .release = my_release;
+}
+
 
 static int __init template_init(void)
 {
 	printk("Hello World, here is your module speaking hohhah\n");
 	printk("this is the seeeeecond line!!\n");
 	printk("and the third----\n");
+	cdev_init(&my_cdev, &my_fops);
+	cl = class_create(THIS_MODULE, "driver-gamepad");
+	device_create(cl, NULL, devno, NULL, "driver-gamepad"); 
 	return 0;
 }
 
-/*
- * template_cleanup - function to cleanup this module from kernel space
- *
- * This is the second of two exported functions to handle cleanup this
- * code from a running kernel
- */
 
 static void __exit template_cleanup(void)
 {
@@ -38,6 +55,6 @@ static void __exit template_cleanup(void)
 module_init(template_init);
 module_exit(template_cleanup);
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+MODULE_DESCRIPTION("gamepad driver");
 MODULE_LICENSE("GPL");
 
